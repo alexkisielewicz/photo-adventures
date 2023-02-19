d3f29cdf5592043e0ab9b7d05aeb8d0749d06ac6
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-19 18:33:44 +0000

    Add post slug to urls to view full posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.BlogPosts.as_view(), name='blog'),
-    path('blog/<slug:slug>/', views.FullPost.as_view(), name='full_post'),
+    path('<slug:slug>/', views.FullPost.as_view(), name='full_post'),
 ]
