6aaf126dfedd6959213cadeb11c0970d60253c6c
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-19 00:06:04 +0000

    Add contact/about/blog pages and urls

--- blog/models.py
+++ blog/models.py
@@ -4,15 +4,17 @@ from cloudinary.models import CloudinaryField
 
 # Tuple for post status
 STATUS = (
-    (0, "Draft"), (1, "Published")
+    (0, 'Draft'), (1, 'Published')
 )
 
 
 class Post(models.Model):
     POST_CATEGORIES = (
         ('adventure', 'Adventure'),
+        ('travel', 'Travel'),
         ('nature', 'Nature'),
         ('landscape', 'Landscape'),
+        ('aerial', 'Aerial'),
         ('wildlife', 'Wildlife'),
         ('street', 'Street'),
         ('architecture', 'Architecture'),
@@ -21,21 +23,21 @@ class Post(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=50, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="photo_adventures")
-    category = models.CharField(choices=POST_CATEGORIES, max_length=20, default="adventure")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='photo_adventures')
+    category = models.CharField(choices=POST_CATEGORIES, max_length=20, default='adventure')
     excerpt = models.TextField(blank=True)
-    featured_image = CloudinaryField("image", default='placeholder')
+    featured_image = CloudinaryField('image', default='placeholder')
     location = models.CharField(max_length=100, unique=False)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name="post_likes", blank=True)
-    featured = models.BooleanField(default=False, verbose_name="featured post")
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    featured = models.BooleanField(default=False, verbose_name='featured post')
 
     # Descending order of Posts
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ['-created_on']
 
     # Method to return string object
     def __str__(self):
@@ -47,7 +49,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
@@ -59,4 +61,4 @@ class Comment(models.Model):
         ordering = ['created_on']
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return f'Comment {self.body} by {self.name}'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class BlogPosts(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('created_on')
+    template_name = 'blog.html'
+    paginate_by = 8
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
