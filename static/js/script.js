669a27a2b1898b4e562b1f2ee9fda0b68fa58782
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-20 12:25:25 +0000

    Initialize tooltip component

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,12 @@
+// set carousel image as carousel-item's background and streches it to cover viewport
 $.each( jQuery('.carousel .carousel-item'), function( i, val ) {
     $(this).css('background-image','url('+$(this).find('img').attr('src')+')').css('background-size','cover').find('img').css('visibility','hidden');
   });
+
+
+// Tooltip for heart icons in full_post template
+  $(document).ready(function(){
+    $('[data-toggle="tooltip-like"]').tooltip({
+        placement: 'left'
+      }); 
+});
