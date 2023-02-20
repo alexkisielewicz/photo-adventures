ea1f20d0b8c9fa543585a0e371ff74c31a9c168b
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-20 23:15:15 +0000

    Adjust templates by spacing elements

--- static/js/script.js
+++ static/js/script.js
@@ -10,3 +10,27 @@ $.each( jQuery('.carousel .carousel-item'), function( i, val ) {
         placement: 'left'
       }); 
 });
+
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset;
+
+if (currentScroll <= 0) {
+  body.classList.remove('scroll-up')
+}
+
+if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+  body.classList.remove('scroll-up')
+  body.classList.add('scroll-down')
+} 
+
+if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
+  body.classList.remove('scroll-down')
+  body.classList.add('scroll-up')
+} 
+
+  lastScroll = currentScroll;
+})
+
