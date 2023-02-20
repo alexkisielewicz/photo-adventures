f80961ad52087f54313a653cbcf0323b6d11ac12
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-20 12:27:02 +0000

    Add liking feature to blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.BlogPosts.as_view(), name='blog'),
     path('<slug:slug>/', views.FullPost.as_view(), name='full_post'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -75,3 +76,16 @@ class FullPost(View):
                 'comment_form': CommentForm()
             },
         )
+
+
+class PostLike(View):
+
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+        
+        return HttpResponseRedirect(reverse('full_post', args=[slug]))
