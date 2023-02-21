3515ce57584553371eddbd895df84c261215cf40
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-21 23:50:21 +0000

    Edit scroll navbar function

--- static/js/script.js
+++ static/js/script.js
@@ -1,36 +1,40 @@
 // set carousel image as carousel-item's background and streches it to cover viewport
 $.each( jQuery('.carousel .carousel-item'), function( i, val ) {
-    $(this).css('background-image','url('+$(this).find('img').attr('src')+')').css('background-size','cover').find('img').css('visibility','hidden');
-  });
+  $(this).css('background-image','url('+$(this).find('img').attr('src')+')').css('background-size','cover').find('img').css('visibility','hidden');
+});
 
 
 // Tooltip for heart icons in full_post template
-  $(document).ready(function(){
-    $('[data-toggle="tooltip-like"]').tooltip({
-        placement: 'left'
-      }); 
+$(document).ready(function(){
+  $('[data-toggle="tooltip-like"]').tooltip({
+      placement: 'left'
+    }); 
 });
 
 const body = document.body;
 let lastScroll = 0;
 
 window.addEventListener('scroll', () => {
-  const currentScroll = window.pageYOffset;
+const currentScroll = window.pageYOffset;
 
 if (currentScroll <= 0) {
-  body.classList.remove('scroll-up')
+body.classList.remove('scroll-up')
 }
 
 if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
-  body.classList.remove('scroll-up')
-  body.classList.add('scroll-down')
+body.classList.remove('scroll-up')
+body.classList.add('scroll-down')
 } 
 
 if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
-  body.classList.remove('scroll-down')
-  body.classList.add('scroll-up')
+body.classList.remove('scroll-down')
+body.classList.add('scroll-up')
 } 
 
-  lastScroll = currentScroll;
+lastScroll = currentScroll;
 })
 
+$('#summernote').summernote({
+  fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Merriweather'],
+  fontNamesIgnoreCheck: ['Merriweather']
+});
