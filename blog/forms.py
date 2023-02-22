6c3416f7aa2a762cf8280fa4bd675d22177a8086
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-22 21:47:55 +0000

    Add PostForm to forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, Post
 from django import forms
 
 
@@ -6,3 +6,18 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+        
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'slug',
+            'category',
+            'excerpt',
+            'featured_image',
+            'location',
+            'content',
+            ]
+
