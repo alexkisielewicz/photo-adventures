78d3aa6afe4e76d768b4f7aed31fc97e605d09b0
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-24 16:04:12 +0000

    Change path for summernote

--- photoadventures/urls.py
+++ photoadventures/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from blog import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
