237cbe2c0f3094d34d45a0deae0aa44a6b119f66
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-25 19:34:50 +0000

    Add tags to admin panel display

--- blog/admin.py
+++ blog/admin.py
@@ -9,10 +9,13 @@ class PostAdmin(SummernoteModelAdmin):
 
     prepopulated_fields = {'slug': ('title', )}
     list_filter = ('status', 'author', 'category', 'created_on')
-    list_display = ('title', 'slug', 'author', 'category', 'featured', 'status', 'created_on')
+    list_display = ('title', 'slug', 'author', 'category', 'get_tags', 'featured', 'status', 'created_on')
     search_fields = ('title', 'content')
     summernote_fields = ('content')
 
+    # method to add tags to admin panel view as described in Taggit docs
+    def get_tags(self, obj):
+        return ', '.join(o for o in obj.tags.names())
 
 # customize comments display in admin panel
 @admin.register(Comment)
@@ -25,3 +28,4 @@ class CommentAdmin(admin.ModelAdmin):
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
+
