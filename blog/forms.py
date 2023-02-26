2b07f2a1fb02d5e3ebf4416e05047c85e41b4287
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-26 15:59:11 +0000

    Remove feature save_draft

--- blog/forms.py
+++ blog/forms.py
@@ -1,32 +1,30 @@
 from .models import Comment, Post
 from django import forms
 from django_summernote.widgets import SummernoteWidget
+from taggit.forms import TagField
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
-        
+
 
 class PostForm(forms.ModelForm):
-    STATUS_CHOICES = (
-        (0, 'Save as draft'),
-        (1, 'Publish now'),
-    )
-    status = forms.ChoiceField(choices=STATUS_CHOICES, widget=forms.RadioSelect)
 
     class Meta:
         model = Post
         fields = [
-        'title',
-        'category',
-        'excerpt',
-        'featured_image',
-        'location',
-        'content',
-        'status',
-    ]
+            'title',
+            'slug',
+            'category',
+            'tags',
+            'excerpt',
+            'featured_image',
+            'location',
+            'content',
+            'status',
+        ]
         widgets = {
             'content': SummernoteWidget(),
         }
