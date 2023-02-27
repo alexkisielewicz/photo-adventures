b0234dc7693d03c26f7ab342a592ab86acf5efe2
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-27 16:19:27 +0000

    Organize code inside document.ready function

--- static/js/script.js
+++ static/js/script.js
@@ -1,60 +1,58 @@
 // set carousel image as carousel-item's background and streches it to cover viewport
 $(document).ready(function () {
+
   $.each(jQuery('.carousel .carousel-item'), function (i, val) {
     $(this).css('background-image', 'url(' + $(this).find('img').attr('src') + ')').css('background-size', 'cover').find('img').css('visibility', 'hidden');
   });
-});
 
-
-// Tooltip for heart icons in full_post template
-$(document).ready(function () {
+  // Tooltip for heart icons in full_post template
   $('[data-toggle="tooltip-like"]').tooltip({
     placement: 'left'
   });
-});
 
-function scrollNavbar() {
-  const body = document.body;
-  let lastScroll = 0;
+  
+  $('.delete-post-btn').click(function (event) {
+    var postId = $(this).data('post-id');
+    $('#deleteForm input[name="post_id"]').val(postId);
+  });
 
-  window.addEventListener('scroll', () => {
-    const currentScroll = window.pageYOffset;
 
-    if (currentScroll <= 0) {
-      body.classList.remove('scroll-up')
-    }
 
-    if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
-      body.classList.remove('scroll-up')
-      body.classList.add('scroll-down')
-    }
-
-    if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
-      body.classList.remove('scroll-down')
-      body.classList.add('scroll-up')
-    }
-
-    lastScroll = currentScroll;
-  })
-}
-
-$(document).ready(function() {
+  function scrollNavbar() {
+    const body = document.body;
+    let lastScroll = 0;
+  
+    window.addEventListener('scroll', () => {
+      const currentScroll = window.pageYOffset;
+  
+      if (currentScroll <= 0) {
+        body.classList.remove('scroll-up')
+      }
+  
+      if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+        body.classList.remove('scroll-up')
+        body.classList.add('scroll-down')
+      }
+  
+      if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
+        body.classList.remove('scroll-down')
+        body.classList.add('scroll-up')
+      }
+  
+      lastScroll = currentScroll;
+    })
+  }
+  
   scrollNavbar();
+  
 });
 
-
+// ADD STATEMENT - IF MODAL EXISTS...
 const deleteConfirmationModal = new bootstrap.Modal(document.getElementById('deleteModal'))
 document.querySelector('[data-target="#deleteModal"]').addEventListener('click', () => {
   deleteConfirmationModal.show()
 })
 
-
-$(document).ready(function () {
-  $('.delete-post-btn').click(function (event) {
-    var postId = $(this).data('post-id');
-    $('#deleteForm input[name="post_id"]').val(postId);
-  });
-});
 
 // About page bootstrap accordion
 const faqAccordion = new bootstrap.Accordion(document.getElementById('faqAccordion'));
