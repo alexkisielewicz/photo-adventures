174055155d00b2a776774335f308e41e911ba04d
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-27 21:06:00 +0000

    Create new file to store const variables

---
+++ blog/constants.py
@@ -0,0 +1,19 @@
+# Tuples for status choices for model post to allow staff moderate posts
+# and to allow users save the drafts
+STATUS = (
+    (0, 'Draft'),
+    (1, 'Sent for moderation'),
+    (2, 'Published'),
+    )
+
+# categories for model Post
+POST_CATEGORIES = (
+        ('adventure', 'Adventure'),
+        ('travel', 'Travel'),
+        ('nature', 'Nature'),
+        ('landscape', 'Landscape'),
+        ('aerial', 'Aerial'),
+        ('wildlife', 'Wildlife'),
+        ('street', 'Street'),
+        ('architecture', 'Architecture'),
+    )
